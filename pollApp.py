a7d46efbcb375a41127241dc487c7461497b7b15
Castro-D <castro.dh91@gmail.com>
2020-12-01 08:02:45 -0300

    agrega argumentos de encuesta
    La encuesta debe tener una pregunta y varias preguntas, como maximo 4 y como minimo 1

---
+++ pollApp.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from Modelos import db
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config.from_object('config')
+db.init_app(app)
+db.create_all(app=app)
+
+encuesta_put_args = reqparse.RequestParser()
+encuesta_put_args.add_argument("pregunta", type=str, help="pregunta de la encuesta", required=True)
+encuesta_put_args.add_argument("respuesta1", type=str, help="respuestas de encuesta", required=True)
+encuesta_put_args.add_argument("respuesta2", type=str, help="respuestas de encuesta")
+encuesta_put_args.add_argument("respuesta3", type=str, help="respuestas de encuesta")
+encuesta_put_args.add_argument("respuesta4", type=str, help="respuestas de encuesta")
+
+
+class Encuesta(Resource):
+    def get(self, encuesta_id):
+        return encuestas[encuesta_id]
+
+
+api.add_resource(Encuesta, "/encuesta/<int:encuesta_id>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
